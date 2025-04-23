c338ae6e4a8201a89b48f3d92e0cb871682def7d
thisisrow <prathameshmishra2020@gmail.com>
2025-04-24 00:14:20 +0530

    add: icon in brouser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,15 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from './providers';
 import { Toaster } from '@/components/ui/toaster';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Prathamesh Mishra | Full Stack Developer',
   description: 'Portfolio of Prathamesh Mishar, a Full Stack Web Developer specializing in React, Next.js, and Node.js',
+  icons: {
+    icon: 'https://avatars.githubusercontent.com/u/175528783?v=4',
+  },
 };
 
 export default function RootLayout({
@@ -19,9 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <link rel="icon" href="https://avatars.githubusercontent.com/u/175528783?v=4" />
-      </Head>
       <body className={inter.className}>
         <Providers>
           {children}
